cfd8a452bfeebcdf700963703260f934580ec9ca
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2017-11-21 12:32:02 -0500

    Utilizamos un componente local en un componente global

---
+++ src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <!--siempre los templates de componentes deben tener un elemento raiz-->
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data:function(){
+      return {
+        status:'Critical'
+      }
+    },
+    methods:{
+      changeStatus(){
+        this.status = 'Normal'
+      }
+    }
+  }
+</script>
